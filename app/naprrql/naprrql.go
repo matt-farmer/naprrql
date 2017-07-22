4adffca05003b634335e665ab19c8a3f00628e9c
matt-farmer <mattfarmer@matts-MacBook-Air.local>
2017-07-22 13:15:13 +1000

    versions with kv store backend
    most important resolvers implemented

--- app/naprrql/naprrql.go
+++ app/naprrql/naprrql.go
@@ -6,30 +6,72 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
+	"os/signal"
 	"runtime"
+	"syscall"
 
 	"github.com/matt-farmer/naprrql"
 )
 
 var ingest = flag.Bool("ingest", false, "forces re-load of data from results file.")
 
-// var webonly = flag.Bool("webonly", false, "just launch web data explorer")
-
 func main() {
 
 	flag.Parse()
 
-	// ingest results data
+	// shutdown handler
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		closeDB()
+		os.Exit(1)
+	}()
+
+	// ingest results data, and exit to save memory
 	if *ingest {
+		clearDBWorkingDirectory()
 		naprrql.IngestResultsFile("master_nap.xml.zip")
+		closeDB()
+		os.Exit(1)
+	} else {
+		// launch sif-ql service
+		go naprrql.RunQLServer()
+		// launch ui service
 	}
 
-	// launch sif-ql service
-	// wait group?
-	go naprrql.RunQLServer()
+	// wait for shutdown
+	for {
+		runtime.Gosched()
+	}
 
-	// launch ui service
+}
 
-	runtime.Goexit()
+//
+// ensure clean shutdown of data store
+//
+func closeDB() {
+	log.Println("Closing datastore...")
+	naprrql.GetDB().Close()
+	log.Println("Datastore closed.")
+}
+
+func clearDBWorkingDirectory() {
+
+	// remove existing logs and recreate the directory
+	err := os.RemoveAll("kvs")
+	if err != nil {
+		log.Println("Error trying to reset datastore working directory: ", err)
+	}
+	createDBWorkingDirectory()
+}
+
+func createDBWorkingDirectory() {
+	err := os.Mkdir("kvs", os.ModePerm)
+	if !os.IsExist(err) && err != nil {
+		log.Fatalln("Error trying to create datastore working directory: ", err)
+	}
 
 }
--- datastore.go
+++ datastore.go
@@ -3,45 +3,40 @@
 package naprrql
 
 import (
-	"bytes"
+	"fmt"
 	"log"
-	"os"
 
-	"github.com/dgraph-io/badger"
 	"github.com/nsip/nias2/naprr"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/util"
 )
 
-var kv *badger.KV
+var db *leveldb.DB
+var dbOpen bool = false
+
 var ge = naprr.GobEncoder{}
 
-func GetKV() *badger.KV {
-	return kv
-}
-
-func init() {
-	kv = openDB()
-}
-
-//
-// open the badger kv store
-//
-func openDB() *badger.KV {
-
-	log.Println("creating data-store directory...")
-	err := os.Mkdir("kvs", os.ModePerm)
-	if err != nil && !os.IsExist(err) {
-		log.Println("Error trying to create datastore working directory", err)
+func GetDB() *leveldb.DB {
+	if !dbOpen {
+		log.Println("DB not initialised. Opening...")
+		openDB()
 	}
+	return db
+}
 
-	log.Println("opening database...")
-	opt := badger.DefaultOptions
-	opt.Dir = "kvs"
-	opt.ValueDir = "kvs"
-	kv, dbErr := badger.NewKV(&opt)
+//
+// open the kv store, this must be called before any access is attempted
+//
+func openDB() {
+
+	workingDir := "kvs"
+
+	var dbErr error
+	db, dbErr = leveldb.OpenFile(workingDir, nil)
 	if dbErr != nil {
 		log.Fatalln("DB Create error: ", dbErr)
 	}
-	return kv
+	dbOpen = true
 }
 
 //
@@ -49,46 +44,52 @@ func openDB() *badger.KV {
 // can be used in a Get operation to retreive the
 // desired object
 //
-func getObjectReferences(keyPrefix string) [][]byte {
+func getIdentifiers(keyPrefix string) []string {
 
-	itrOpt := badger.IteratorOptions{
-		PrefetchSize: 1000,
-		FetchValues:  true,
-		Reverse:      false,
-	}
-	itr := kv.NewIterator(itrOpt)
+	db = GetDB()
+	objIDs := make([]string, 0)
 
 	searchKey := []byte(keyPrefix + ":")
-	objIDs := make([][]byte, 0)
-	for itr.Seek(searchKey); itr.Valid(); itr.Next() {
-		if !bytes.Contains(itr.Item().Key(), searchKey) {
-			break
-		}
-		objIDs = append(objIDs, itr.Item().Value())
+	iter := db.NewIterator(util.BytesPrefix(searchKey), nil)
+	for iter.Next() {
+		id := fmt.Sprintf("%s", iter.Value())
+		objIDs = append(objIDs, id)
+		// break
 	}
+	iter.Release()
+	err := iter.Error()
+	if err != nil {
+		log.Println("Iterator error: ", err)
+	}
+
 	return objIDs
 }
 
-func getObjectForKeys(objIDs [][]byte) ([]interface{}, error) {
-	summaries := []interface{}{}
+//
+// get objects stored against the list of keys
+//
+func getObjects(objIDs []string) ([]interface{}, error) {
+
+	db = GetDB()
+	objects := []interface{}{}
 
 	for _, objID := range objIDs {
-		// fmt.Printf("%s - %+v\n", key, val)
-		var item badger.KVItem
-		var summary interface{}
-		if err := kv.Get(objID, &item); err != nil {
+		var object interface{}
+		data, err := db.Get([]byte(objID), nil)
+		if err != nil {
 			log.Println("Cannot find object with key: ", string(objID))
-			return summaries, err
+			return objects, err
 		}
-		if err := ge.Decode(item.Value(), &summary); err != nil {
+		err = ge.Decode(data, &object)
+		if err != nil {
 			log.Println("Cannot decode object with key: ", objID, err)
-			return summaries, err
+			return objects, err
 		}
-		summaries = append(summaries, summary)
+		objects = append(objects, object)
 
 	}
 
-	return summaries, nil
+	return objects, nil
 
 }
 
